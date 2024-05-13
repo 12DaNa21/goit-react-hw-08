ca2af9ed82155b0ddcc57f5c3bd852c444d41ba1
12DaNa21 <145206474+12DaNa21@users.noreply.github.com>
2024-05-13 10:03:43 +0300

    update hw7

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,14 @@
 import css from "./ContactList.module.css";
 import Contact from "../Contact/Contact";
 import { useSelector } from "react-redux";
-import { selectContacts } from "../../redux/contactsSlice";
-import { selectNameFilter } from "../../redux/filtersSlice";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
+
 
 export default function ContactList() {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-
-  const visibleContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const contacts = useSelector(selectFilteredContacts)
   return (
     <ul className={css.list}>
-      {visibleContacts.map((contact) => (
+      {contacts.map((contact) => (
         <li key={contact.id} className={css.item}>
           <Contact data={contact} />
         </li>
@@ -21,3 +16,4 @@ export default function ContactList() {
     </ul>
   );
 }
+
